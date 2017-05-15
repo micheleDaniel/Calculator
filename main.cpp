ea8adcdc6c07d3a44dc72b4666c9608b996a180d
root <kalimetasploiter@gmail.com>
2017-05-15 20:56:41 +0200

    Implementation of addtion function

--- main.cpp
+++ main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
-using namespace std;
 
-int addition();
+void addition();
 int sottraction();
 int moltiplication();
 int division();
+void line();
 
 int main() {
-	cout <<"CALCULATOR"<<endl;
-
+	std::cout <<"------CALCULATOR------"<<std::endl;
+	line();
+	addition();		
     return 0;
 }
 
 int moltiplication(int res){
 	int a, b;
-	cout << "First Number ";
-	cin >>a;
-	cout << "Second Number ";
-	cin >>b;
+	std::cout << "First Number ";
+	std::cin >>a;
+	std::cout << "Second Number ";
+	std::cin >>b;
 	res = a*b;
 	return res;
+}
+
+void addition(){
+	int n1, n2,res;
+	std::cout << "Insert first number: ";
+	std::cin >> n1;
+	std::cout << "Insert second number: ";
+	std::cin >> n2;
+	res = n1 + n2;
+	std::cout << "The addtion reults is: " << res << std::endl;
+}
+
+void line(){
+	std::cout<<std::endl;
 }
